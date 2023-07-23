e4e17b279b4c537611dc2e8561c680d8a981f1c7
Eharry28 <119737537+Eharry28@users.noreply.github.com>
2023-07-23 13:16:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const tx1= document.getElementById('Text1').value;
-const tx2= document.getElementById('Text2').value;
-
-tx1.addEventListener('change',disBtn);
-tx2.addEventListener('change',disBtn);
-
-function disBtn() {
-    if(tx1!=""&&tx2!=""){
-        document.getElementById('clickbtn').disabled = false;
-    }else{
-        document.getElementById('clickbtn').disabled = true;
-    }    
-}
+    window.onload=function(){
+        const tx1= document.getElementById('Text1');
+        const tx2= document.getElementById('Text2');
+        console.log(tx1);
+        tx1.addEventListener('change',disBtn);
+        tx2.addEventListener('change',disBtn);
+    }
+    function disBtn() {
+        if(tx1.value!=""&&tx2.value!=""){
+            document.getElementById('clickbtn').disabled = false;
+        }else{
+            document.getElementById('clickbtn').disabled = true;
+        }    
+    }
 
